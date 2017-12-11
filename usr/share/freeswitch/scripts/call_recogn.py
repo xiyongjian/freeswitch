045d821ba584fa624c17178ca0bda9f3d81d3e1f
root <root@localhost.localdomain>
2017-12-11 17:34:13 -0500

    python module GSR and post cdr to REST API

--- usr/share/freeswitch/scripts/call_recogn.py
+++ usr/share/freeswitch/scripts/call_recogn.py
@@ -5,8 +5,12 @@ import speech_recognition as sr
 import sys
 from os import path
 
+import requests
+import json
+from datetime import datetime
 
 rec_file = "";
+cdr_api_url = "http://192.168.151.101:8080/ngDesk-fsapi/cdr"
 
 def hangup_hook(uuid) :
     consoleLog("info", "call_recogn hangup_hook - uuid : %s\n" % str(uuid))
@@ -21,6 +25,7 @@ def recogn_file(wav_file) :
     rsl_file = wav_file + ".txt"
     
     # recognize speech using Google Speech Recognition
+    result = ""
     try:
 	result = r.recognize_google(audio)
         consoleLog("info", "call_recogn recogn_file - Google Speech Recognition result : %s\n" % str(result))
@@ -31,6 +36,7 @@ def recogn_file(wav_file) :
         consoleLog("info", "call_recog recogn_file - Google Speech Recognition could not understand audio\n")
     except sr.RequestError as e:
         consoleLog("info", "call_recog recogn_file - Could not request results from Google Speech Recognition service; {0}\n".format(e))
+    return result
 
 
 def handler(session,args):
@@ -49,4 +55,38 @@ def handler(session,args):
 def fsapi(session,stream,event,args): 
     rec_file = session.getVariable("recording_file")
     consoleLog("info", "call_recogn fsapi - recogn recording_file: %s\n" % rec_file)
-    recogn_file(rec_file)
+    transcript = recogn_file(rec_file)
+
+    # prepare JSON payload
+    cdr = {}
+    cdr['DOMAIN'] = session.getVariable("domain")
+    cdr['DOMAIN_NAME'] = session.getVariable("domain_name")
+    cdr['FROM_USER_ID'] = session.getVariable("caller_id_number")
+
+    to_users = [];
+    to_users.append({'TO_USER_ID' : session.getVariable("destination_number")})
+    cdr['TO_USERS'] = to_users
+    
+    recording_time = session.getVariable("recording_time")
+    cdr['DATE_CREATED'] = recording_time
+    
+    d00 = datetime.strptime(recording_time, '%Y-%m-%d-%H-%M-%S')
+    d01 =  datetime.now();
+    cdr['DURATION'] = str(int((d01-d00).total_seconds()))
+    
+    cdr['TRANSCRIPT'] = transcript
+
+    payld = json.dumps(cdr, indent=4)
+
+    # write JSON to file first
+    with open(rec_file + ".json", 'w') as the_file:
+        the_file.write(payld)
+
+    # post JSON to REST API (CDR)
+    consoleLog("info", "json payload : %s\n" % payld)
+    consoleLog("info", "post json to url : %s" % cdr_api_url)
+    response = requests.post(cdr_api_url,data=payld)
+    consoleLog("info", "post result status : %d" % response.status_code)
+    consoleLog("info", "post result json : %s" % json.dumps(response.json(), indent=4))
+
+

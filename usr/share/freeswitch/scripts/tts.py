59ae9e78536931ca431b26d4487ffa3cad8c0ac0
root <root@jessie.1028>
2017-12-14 13:06:40 -0700

    tts ivr improved

--- usr/share/freeswitch/scripts/tts.py
+++ usr/share/freeswitch/scripts/tts.py
@@ -17,23 +17,29 @@ def handler(session,args):
     consoleLog("info", "setup variables")
     consoleLog("info", "start speak")
     session.execute("speak","This is testing for TTS working properly")
-    session.execute("speak","say something...")
-    rtn = session.recordFile(wav_file, 20, 500, 3);
 
-    r = sr.Recognizer()
-    with sr.AudioFile(wav_file) as source:
-        audio = r.record(source)  # read the entire audio file
+    msg = ""
+    while "quit" not in msg :
+        session.execute("speak","say something, let me guess what you said...")
+        rtn = session.recordFile(wav_file, 20, 500, 3);
 
-    result = ""
-    try:
-        result = r.recognize_google(audio)
-        consoleLog("info", "call_recogn recogn_file - Google Speech Recognition result : %s\n" % str(result))
-    except sr.UnknownValueError:
-        consoleLog("info", "call_recog recogn_file - Google Speech Recognition could not understand audio\n")
-    except sr.RequestError as e:
-        consoleLog("info", "call_recog recogn_file - Could not request results from Google Speech Recognition service; {0}\n".format(e))
+        r = sr.Recognizer()
+        with sr.AudioFile(wav_file) as source:
+            audio = r.record(source)  # read the entire audio file
 
-    session.execute("speak","you just said something like : " + str(result))
+        result = ""
+        try:
+            result = r.recognize_google(audio)
+            consoleLog("info", "call_recogn recogn_file - Google Speech Recognition result : %s\n" % str(result))
+        except sr.UnknownValueError:
+            consoleLog("info", "call_recog recogn_file - Google Speech Recognition could not understand audio\n")
+        except sr.RequestError as e:
+            consoleLog("info", "call_recog recogn_file - Could not request results from Google Speech Recognition service; {0}\n".format(e))
+
+        session.execute("speak","you just said something like : " + str(result))
+        session.execute("sleep","1000")
+        msg = str(result);
+
 
 
 

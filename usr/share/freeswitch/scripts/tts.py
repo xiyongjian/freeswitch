00c52a2be16ff0fa2775287c90af3f005ce74ae9
root <root@webrtc-da3-1.ngdesk.com>
2017-12-14 15:11:02 -0500

    tts ivr function

---
+++ usr/share/freeswitch/scripts/tts.py
@@ -0,0 +1,46 @@
+from freeswitch import *
+
+import speech_recognition as sr
+import sys
+from os import path
+
+import json
+from datetime import datetime
+
+
+def handler(session,args):
+    wav_file = "/tmp/recording.wav"
+    consoleLog("info", "using wave file %s to recogn-replay\n" % wav_file)
+
+    consoleLog("info", "tts.py started")
+    session.execute("set","tts_engine=flite")
+    session.execute("set","tts_voice=kal")
+    consoleLog("info", "setup variables")
+    consoleLog("info", "start speak")
+    session.execute("speak","This is testing for TTS working properly")
+    # session.execute("set_zombie_exec")
+
+    msg = ""
+    while "quit" not in msg :
+        session.execute("speak","say something, let me guess what you said...")
+        rtn = session.recordFile(wav_file, 20, 500, 3);
+
+        r = sr.Recognizer()
+        with sr.AudioFile(wav_file) as source:
+            audio = r.record(source)  # read the entire audio file
+
+        result = ""
+        try:
+            result = r.recognize_google(audio)
+            consoleLog("info", "call_recogn recogn_file - Google Speech Recognition result : %s\n" % str(result))
+            session.execute("speak","you just said something like : " + str(result))
+            msg = str(result);
+        except sr.UnknownValueError:
+            consoleLog("info", "call_recog recogn_file - Google Speech Recognition could not understand audio\n")
+            msg = "quit"
+        except sr.RequestError as e:
+            consoleLog("info", "call_recog recogn_file - Could not request results from Google Speech Recognition service; {0}\n".format(e))
+            msg = "quit"
+
+        session.execute("sleep","1000")
+
